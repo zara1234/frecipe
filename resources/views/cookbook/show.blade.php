b085959b88e1671f9b4ea4e16e3b4c4858604156
Sarah Mühleder <>
2019-01-29 17:28:49 +0100

    fix single recipe view

--- resources/views/cookbook/show.blade.php
+++ resources/views/cookbook/show.blade.php
@@ -1,21 +1,25 @@
-<h1>{{ $recipe->name }}</h1>
+@extends('master')
 
-<h2>Ingredients</h2>
-<ul>
-@foreach ($recipe->ingredients as $ingredient)
-    @if ($ingredient->amount === true)
-        <li>a bit of {{ $ingredient->name }}</li>
-    @else
-        <li>{{ $ingredient->amount }} {{ $ingredient->unit }} {{ $ingredient->name }}</li>
-    @endif
-@endforeach
-</ul>
+@section('body')
+        <h1>{{ $recipe->name }}</h1>
 
-<h2>How To</h2>
-<ol>
-    @foreach($recipe->preparation as $preparation)
-        <li>{{ $preparation }}</li>
-    @endforeach
-</ol>
+        <h2>Ingredients</h2>
+        <ul>
+                @foreach ($recipe->ingredients as $ingredient)
+                        @if ($ingredient->amount === true)
+                                <li>a bit of {{ $ingredient->name }}</li>
+                        @else
+                                <li>{{ $ingredient->amount }} {{ $ingredient->unit }} {{ $ingredient->name }}</li>
+                        @endif
+                @endforeach
+        </ul>
 
-<a href="{{ url()->previous() }}" class="back">Back To Cookbook</a>
+        <h2>How To</h2>
+        <ol>
+                @foreach($recipe->preparation as $preparation)
+                        <li>{{ $preparation }}</li>
+                @endforeach
+        </ol>
+
+        <a href="{{ url()->previous() }}" class="back">Back To Cookbook</a>
+@endsection
